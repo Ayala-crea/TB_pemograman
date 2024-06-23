222dcfdd7128ab0b0b3e529b64523c6bb5a8a9f8
ayalarifki <ayalarifki@gmail.com>
2024-06-23 22:33:11 +0700

    cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,7 +7,7 @@ import (
 // CORSMiddleware menambahkan header CORS ke setiap request
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:8089")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
